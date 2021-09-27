5a71e8d988901a775a6e36b65c1e1d7bd0fc7277
x <xxx@tsststs.com>
2021-09-26 21:01:52 -0400

    hw

---
+++ hw2-2.py
@@ -0,0 +1,10 @@
+# Author MEE (AMDG) 9/25/21
+
+freethrows_scored = 1
+twopoints = 4
+threepoints = 7
+
+overallpoints = (freethrows_scored) + (twopoints * 2) + (threepoints * 3)
+
+print("Overall Points scored this game is " + str(overallpoints))
+
